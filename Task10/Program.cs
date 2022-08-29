e2db556ff0d88996f104ac9b630d0b07d8a65d9a
Leonid Ivanov <leonidivanov01@gmail.com>
2022-08-29 19:17:37 +0600

    Add task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+Console.WriteLine("Введите число: ");
+int threeDigitNum = Convert.ToInt32(Console.ReadLine());
+
+if (threeDigitNum > 99 && threeDigitNum < 1000)
+{
+    Console.WriteLine((threeDigitNum / 10) % 10);
+}
+else
+{
+    Console.WriteLine("Введите другое число!");
+}
